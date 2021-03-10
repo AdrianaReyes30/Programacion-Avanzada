b6d6c0a99e601df844d17fe3b176363b32c98802
AdrianaReyes30 <79930228+AdrianaReyes30@users.noreply.github.com>
2021-03-10 11:33:39 -0600

    ShoppingCart01
    Tema 3.3 Datos Textuales

---
+++ ShoppingCart01.java
@@ -0,0 +1,19 @@
+
+package shoppingcart01;
+
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName ="purcharse";
+        String itemDesc= "shirt";
+        String message= "alex wants to" +custName +" a " +itemDesc;
+        
+        System.out.println(message); 
+        
+        // Assign the message variable 
+        
+        
+        // Print and run the cod
+        
+    }
+}
