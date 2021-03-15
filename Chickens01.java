361f8ee236456d2c5a78deb5f771db1a4281142a
AdrianaReyes30 <79930228+AdrianaReyes30@users.noreply.github.com>
2021-03-15 12:04:08 -0600

    3_2 Datos Numericos

---
+++ Chickens01.java
@@ -0,0 +1,23 @@
+
+package chickens01;
+
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put yout code here
+        int eggsPerChicken=4;
+        int chickenCount=8;
+        int totalEggsMon;
+        totalEggsMon=eggsPerChicken*chickenCount;
+        chickenCount++;
+        int totalEggsTue;
+        totalEggsTue=eggsPerChicken*chickenCount+totalEggsMon;
+        chickenCount=chickenCount/2;
+        int totalEggsWen;
+        totalEggsWen=chickenCount*eggsPerChicken+totalEggsTue;
+        int totalEggs;
+        totalEggs=totalEggsWen;
+        System.out.println(chickenCount);
+        
+        System.out.println(totalEggs);
+    }   
+}
